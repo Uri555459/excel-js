ae6a7bd893545f75169e9d337111d91918ee061b
Uri <uri555459@gmail.com>
2022-07-18 16:41:55 +0300

    Added ability to add and delete events

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,49 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector
+  }
 
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    }
+
+    return this.$el.outerHTML.trim()
+  }
+
+  clear() {
+    this.html('')
+    return this
+  }
+
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback)
+  }
+
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback)
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node)
+    } else {
+      this.$el.appendChild(node)
+    }
+
+    return this
+  }
 }
 
-export function $() {
-  return new Dom()
+export function $(selector) {
+  return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
@@ -12,5 +52,5 @@ $.create = (tagName, classes = '') => {
     el.classList.add(classes)
   }
 
-  return el
+  return $(el)
 }
